6d6acb1c3785f7a6f7e6ae8b7a105064bc0ccfd1
kfokusd <93218564+kfokusd@users.noreply.github.com>
2021-11-17 11:09:14 -0800

    Add files via upload

---
+++ quiz7.py
@@ -0,0 +1,22 @@
+# Function: is_prime(n)
+# - Takes 1 parameter, n (integer)
+# - Returns True if n is a prime number; otherwise, returns False
+# TODO: Implement this function
+
+# Function: primelist(min, max)
+# - Takes 2 parameters, min, and max (integer)
+# - Returns a list with prime numbers between (min) and (max) in descending order  
+# - For example,  primelist(3, 10) returns [7, 5]
+# - For example,  primelist(-10, 10) returns [7, 5, 3, 2]
+def primelist(min, max):
+    primelst = []
+
+    # TODO: Implement this function
+    return primelst 
+
+
+# DO NOT change the following code
+print(primelist(3, 10))
+print(primelist(11, 50))
+print(primelist(-10, 10))
+print(primelist(-100, 100))
