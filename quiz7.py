b08ae1f33cd7843748663d54d128e281abbddec5
kfokusd <93218564+kfokusd@users.noreply.github.com>
2022-11-15 10:07:22 -0800

    Updated

--- quiz7.py
+++ quiz7.py
@@ -1,6 +1,6 @@
 # Function: is_prime(n)
 # - Takes 1 parameter, n (integer)
-# - Returns True if n is a prime number; otherwise, returns False
+# - Returns True if n is a prime number (must be greater than 1); otherwise, returns False
 # TODO: Implement this function
 
 # Function: primelist(min, max)
@@ -20,9 +20,3 @@ print(primelist(3, 10))
 print(primelist(11, 50))
 print(primelist(-10, 10))
 print(primelist(-100, 100))
-
-# Expected Output:
-#[7, 5]
-#[47, 43, 41, 37, 31, 29, 23, 19, 17, 13]
-#[7, 5, 3, 2]
-#[97, 89, 83, 79, 73, 71, 67, 61, 59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2]
